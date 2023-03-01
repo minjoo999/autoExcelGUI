72f1b9efdb8b24a48b9692a1d1dec1628339f185
minjoo999 <sjctk1@gmail.com>
2023-03-01 14:08:45 +0900

    내용 확정 버튼 수정, 제목 작성 여부 검증

--- gui.py
+++ gui.py
@@ -16,7 +16,7 @@ class WindowClass(QMainWindow, form_class) :
         # 기본 제목
         self.checkBox.stateChanged.connect(self.defaultTitle)
 
-        # 내용 확정 및 작동 시작 버튼
+        # 내용 확정 및 작동 시작 버튼 (제목이 있어야만 버튼 작동)
         self.fixBtn.clicked.connect(self.fixBtnPush)
         self.startBtn.clicked.connect(self.startBtnPush)
     
@@ -27,15 +27,28 @@ class WindowClass(QMainWindow, form_class) :
 
     # 내용 확정 (시작일자, 종료일자, 제목)
     def fixBtnPush(self):
-        startDate = self.startDate.date()
-        endDate = self.endDate.date()
+        startDate = self.startDate.date().toString("yyyy-MM-dd")
+        endDate = self.endDate.date().toString("yyyy-MM-dd")
         title = self.titleText.toPlainText()
-        print(startDate, endDate, title)
+
+        # 제목을 지어야만 내용확정 가능
+        # 내용 확정 버튼 누르면 내용 변경 막힘
+        if len(title) > 0:
+            self.startDate.setEnabled(False)
+            self.endDate.setEnabled(False)
+            self.titleText.setEnabled(False)
+            print(startDate, endDate, title)
+        else:
+            QMessageBox.warning(self, "경고", "제목을 입력해주세요")
 
     # 정리 시작
     def startBtnPush(self):
-        # start = make.begin
-        print(make.begin())
+
+        # 제목을 지어야만 정리 시작 가능
+        if len(self.titleText.toPlainText()) > 0:
+            print(make.begin())
+        else:
+            QMessageBox.warning(self, "경고", "제목을 입력해주세요")
 
     # 작업 종료 후 완료했습니다 창 만들기
     # or 만들어진 파일 저장된 폴더 띄우기                                                                                                                  
